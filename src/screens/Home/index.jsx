31766dacac42af2956e5a24353c1b161df8f3c1f
CAMPINAS LESTE - ANA RITA GODINHO POUSA PROFESSORA <e018200w10@professor.educacao.sp.gov.br>
2023-08-17 12:22:48 -0300

    inicio da semantica aplicada na home

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -10,43 +10,39 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <>
-      <div className="container-home">
-      <div className="alinhar-home">
-        <div className="seguradora">
-        <h1>Selcione O Tipo de Denuncia:</h1>
-        <div class="agrupar-botões">
-  <span class="botão">
-    <button>
-    <Link to="/anonima" className="no-decoration">
-      <p class="text-responsive text-esquerda">
-        Anônima
-        <img src={patoAnonimo} alt="Pato Anônimo" class="imagem-lado" />
-      </p>
-      </Link>
-    </button>
-  </span>
-          
-          <span className="botão">
-          <button>
-          <Link to="/normal" className="no-decoration">
-            <p className="text-responsive text-direita">Aberta
-            <img src={patoNormal} alt="Pato Anônimo" class="imagem-lado" /></p>
-            </Link>
-          </button>
-          </span>
-          <p>Ou</p>
-          <span className="botão">
-          <button className="custom-button text-responsive">
-          <Link to="/professor" className="no-decoration"><p>Sou Professor
-            <img src={iconePessoa} alt="Pato Anônimo" class="imagem-lado" />
-            </p>
-            </Link>
-          </button>
-          </span>
-          </div>
-          </div>
-        </div>
-      </div>
+      <main className="container-home">
+
+        <section className="tipo-de-denuncia">
+        <h1 className="title-home">Selcione O Tipo de Denuncia:</h1>
+
+          <section className="seguradora">
+                <button class="botão-opções">
+                  <Link to="/anonima" className="no-decoration">
+                    <p class="text-responsive text-esquerda">
+                      Anônima
+                      <img src={patoAnonimo} alt="Pato Anônimo" class="imagem-lado" />
+                    </p>
+                  </Link>
+                </button>
+              
+
+                <button class="botão-opções">
+                  <Link to="/normal" className="no-decoration">
+                    <p className="text-responsive text-direita">Aberta
+                      <img src={patoNormal} alt="Pato Anônimo" class="imagem-lado" />
+                      </p>
+                  </Link>
+                </button>
+              <p>Ou</p>
+                <button class="botão-opções">
+                  <Link to="/professor" className="no-decoration"><p>Sou Professor
+                    <img src={iconePessoa} alt="Pato Anônimo" class="imagem-lado" />
+                  </p>
+                  </Link>
+                </button>
+          </section>
+        </section>
+      </main>
     </>
   );
 };
